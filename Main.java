f5398fd610bc9920241918d77c1a6a13bf73fe4e
Iskhakov-81 <64727382+Iskhakov-81@users.noreply.github.com>
2020-05-16 16:49:08 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,94 +1,84 @@
-import java.util.Scanner;
-     // задание 1
-    // public class Main {
-
-         //public static void main(String[] args) {
-             // write your code here
-       //  }
-    // }
-
+ // второе урок/ задание 1 Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
+ // С помощью цикла и условия заменить 0 на 1, 1 на 0
 public class Main {
-     //задание 2
-
     public static void main(String[] args) {
-        int aa=543232;
-        byte bb=115;
-        short cc=500;
-        long  dd=215112231;
-        char zz='v';
-        float tt=5.8f;
-        double ww=6.9;
-        System.out.println(aa);
-        System.out.println(bb);
-        System.out.println(cc);
-        System.out.println(dd);
-        System.out.println(zz);
-        System.out.println(tt);
-        System.out.println(ww);
 
-
-      // задание 3
-
-        System.out.println("Считаем a * (b + (c / d)) ");
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("введите а");
-        int a = scanner.nextInt();
-        System.out.println("введите b");
-        int b = scanner.nextInt();
-        System.out.println("введите c");
-        int c = scanner.nextInt();
-        System.out.println("введите d");
-        int d = scanner.nextInt();
-        int result= a * (b + (c / d));
-        System.out.println("Получилось =" +result);
-
-
-        // задание 4
-        System.out.println("Проверяем что их сумма лежит в пределах от 10 до 20(включительно), если да – вернуть true, в противном случае – false");
-        System.out.println("введите первое число");
-        int q = scanner.nextInt();
-        System.out.println("введите второе число");
-        int w = scanner.nextInt();
-        int result2=q+w;
-        if (result2>=10 & result<20){
-            System.out.println("Cумма ="+result2 +" лежит между 10 и 20ю");
-        } else {
-            System.out.println("Cумма ="+result2 +" и не лежит между 10 и 20ю");
+        int[] massiv = {1, 1, 0, 0, 1, 0, 1, 1, 0, 1};
+        int maxmasiv = massiv.length;
+        for (int i = 0; i < maxmasiv; i++) {
+            System.out.print(i + "-" + massiv[i] + " ");
         }
-
-       // задание 5
-        System.out.println("Положительное число или отрицательное?");
-        System.out.println("Введите число");
-        int result1=scanner.nextInt();
-        if (result1>=0){
-            System.out.println(result+"=Число положительное");
-        } if (result1<0){
-            System.out.println(result1+"=Число отрицательное ");
+        System.out.println("    ");
+        for (int i = 0; i < maxmasiv; i++) {
+            if (massiv[i] == 1) {
+                massiv[i] = 0;
+            } else massiv[i] = 1;
         }
-
-        // задание 6
-        System.out.println("Введите число");
-        int a1=scanner.nextInt();
-        if (a1<0) {
-            System.out.println("Число отрицательное");
+        for (int i = 0; i < maxmasiv; i++) {
+            System.out.print(i + "-" + massiv[i] + " ");
         }
+    }
+   // задание 2   Задать пустой целочисленный массив размером 8.
+   // С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21
+    public static void mass2() {
 
-        // задание 7
+        int[] arr = new int[8];
+        int j = 0;
+        int mAss2 = arr.length;
+        for (int i = 0; i < mAss2; i++) {
+            System.out.print(i + "-" + arr[i] + " ");
+        }
+        System.out.println(" ");
+        for (int i = 0; i < mAss2; i++, j = j + 3) {
+            arr[i] = j;
+        }
+        for (int i = 0; i < mAss2; i++) {
+            System.out.print(i + "-" + arr[i] + " ");
+        }
+    }
 
-         //static void name1(); {
-            // на этом задании не читается переменная s
-            System.out.println("Как вас зовут?");
-            String s = scanner.nextLine();
-            System.out.println("Привет" + s);
-       // }
-
-
-          // задание 8
-            System.out.println("Введите год");
-            int year=scanner.nextInt();
-            if (!(year % 4 == 0) || ((year % 100 == 0) && !(year % 400 == 0))) System.out.println(year + " г. не высокосный");
-            else System.out.println(year + " г. высокосный");
-
+    //задание 3 Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом,
+    // и числа меньшие 6 умножить на 2
+    public static void mass3() {
+        int[] mas3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        int maxMas = mas3.length;
+        for (int i = 0; i < maxMas; i++) {
+            if (mas3[i] < 6){
+                mas3[i] = mas3[i] * 2;
+            }
+        }
+        for (int i = 0; i < maxMas; i++) {
+            System.out.print(i + "-" + mas3[i] + " ");
+        }
 
     }
+    // задание 4 Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
+     // и с помощью цикла(-ов) заполнить его диагональные элементы единицами
+
+     public static void mass4(){
+         int[][] mas4 = new int[4][4];
+         for (int i = 0; i < mas4.length; i++) {
+             for (int j = 0, j2 = mas4[i].length; j < mas4[i].length; j++, j2--) {
+                 if (i == j || i == (j2 - 1)) mas4[i][j] = 1;
+                 System.out.print(mas4[i][j] + " ");
+             }
+             System.out.print("\r\n");
+         }
+     }
+     // задание 5   Задать одномерный массив и найти в нем минимальный и максимальный элементы
+
+     public static void mass5(){
+         int[] mas5 = {23, 5, 13, 24, 2, 8, 5, 3, 3, 8, 9, 21};
+         int min = mas5[0], max = mas5[0], maxId = 0, minId = 0;
+         for (int i = 0; i < mas5.length; i++) {
+             if (mas5[i] > max) {
+                 max = mas5[i];
+             }
+             if (mas5[i] < min) {
+                 min = mas5[i];
+             }
+         }
+         System.out.println("Максимальный элемент  "  + max);
+         System.out.println("Минимальный элемент " + min);
+     }
 }
